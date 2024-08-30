a8872f7c19a8ccbca3f91f6fa8c53f0a057832d1
Preston Ong <preston4896@me.com>
2024-08-30 10:05:51 +0800

    init commit for x509-cli-demo

---
+++ x509-risczero-cli/README.md
@@ -0,0 +1,70 @@
+# X509 Chain Verifier RiscZero Demo CLI
+
+---
+
+## Summary
+
+Follow these steps to get started with this tool:
+
+0. Install [Rust](https://doc.rust-lang.org/book/ch01-01-installation.html)
+
+1. Export `BONSAI_API_KEY` and `BONSAI_API_URL` values into the shell. If you don't have a Bonsai API key, send a [request](https://docs.google.com/forms/d/e/1FAIpQLSf9mu18V65862GS4PLYd7tFTEKrl90J5GTyzw_d14ASxrruFQ/viewform) for one.
+
+```bash
+export BONSAI_API_KEY="" # see form linked above
+export BONSAI_API_URL="" # provided with your api key
+```
+
+2. Build the program.
+
+```bash
+cargo build --release
+```
+
+---
+
+## CLI Commands
+
+You may run the following command to see available commands.
+
+```bash
+./target/release/demo --help
+```
+
+Outputs:
+
+```bash
+Performs X509 Chain Verification via Bonsai, then submits proof on-chain
+
+Usage: demo [OPTIONS]
+
+Options:
+  -c, --cert-chain <ONE_OR_MORE_PEM_PATH>
+          One or more paths to PEM file(s). If multiple PEM files provided, you must ensure that they are ordered bottom-top from leaf to root
+      --rpc <RPC_URL>
+          Optional: RPC URL [default: https://automata-testnet.alt.technology]
+      --contract <CONTRACT_ADDRESS>
+          Optional: X509 Chain Demo Contract Address [default: 7eD4D92f114A002B5025614b12Ba9F00b40a273d]
+  -w, --wallet-key <WALLET>
+          REQUIRED: if the verify option is enabled, the user must provide an Ethereum Wallet Key
+  -v, --verify
+          Verify the provided cert chain If not specified, attempts to fetch the Journal on-chain
+  -h, --help
+          Print help
+  -V, --version
+          Print version
+```
+
+To submit verification of the X509 Certificate Chain, pass one or more `-c` arguments followed by the path to PEM file(s). You must pass along the `-v` flag to enable the verifier and your wallet key to the `-w` flag. Run the following command for an example:
+
+```bash
+RUST_LOG="info" ./target/release/demo -c ./samples/attestation.pem -w <WALLET_KEY> -v
+```
+
+Upon successful verification, the journal will then be submitted on-chain.
+
+To retrieve an existing journal for a verified X509 Certificate Chain, you may simply omit the `-v` and `-w` flags.
+
+```bash
+./target/release/demo -c ./samples/attestation.pem
+```
---
+++ x509-risczero-cli/src/bin/demo.rs
@@ -0,0 +1,268 @@
+use std::{path::PathBuf, str::FromStr, time::Duration};
+use alloy::{
+    dyn_abi::SolType, network::EthereumWallet, primitives::{Address, Bytes}, providers::ProviderBuilder, rpc::types::TransactionReceipt, signers::{k256::ecdsa::SigningKey, local::PrivateKeySigner}, sol, sol_types::SolValue, transports::http::reqwest::Url
+};
+use risc0_ethereum_contracts::groth16;
+use anyhow::{Error, Result, Context};
+use clap::Parser;
+use bonsai_sdk::alpha as bonsai_sdk;
+
+use x509_parser::prelude::*;
+
+const X509_IMAGE_ID: &str = "a20a7e4d0a232f1a7858439acc777f0601e8f5be7bdeb2cec144c14a779a4424";
+
+type InputBytesType = sol!(bytes[]);
+
+#[derive(Parser)]
+#[command(name = "X509DemoApp")]
+#[command(version = "1.0")]
+#[command(about = "Performs X509 Chain Verification via Bonsai, then submits proof on-chain", long_about = None)]
+struct Cli {
+    /// One or more paths to PEM file(s).
+    /// If multiple PEM files provided, you must ensure that they are ordered bottom-top
+    /// from leaf to root.
+    #[arg(short = 'c', long = "cert-chain", value_name = "ONE_OR_MORE_PEM_PATH")]
+    cert_chain: Vec<PathBuf>,
+
+    /// Optional: RPC URL
+    #[arg(long = "rpc", value_name = "RPC_URL", default_value_t = String::from("https://automata-testnet.alt.technology"))]
+    rpc_url: String,
+
+    /// Optional: X509 Chain Demo Contract Address
+    #[arg(long = "contract", value_name = "CONTRACT_ADDRESS", default_value_t = String::from("7eD4D92f114A002B5025614b12Ba9F00b40a273d"))]
+    address: String,
+
+    /// REQUIRED: if the verify option is enabled,
+    /// The user must provide an Ethereum Wallet Key
+    #[arg(short = 'w', long = "wallet-key")]
+    wallet: Option<String>,
+
+    /// Verify the provided cert chain
+    /// If not specified, attempts to fetch the Journal on-chain
+    #[arg(short = 'v', long = "verify")]
+    verify: bool
+}
+
+struct Chain {
+    pub rpc_url: Url,
+    pub contract_address: Address,
+    pub wallet: Option<EthereumWallet>
+}
+
+impl Chain {
+    fn new(rpc_url_str: &str, address: &str) -> Self {
+        Chain {
+            rpc_url: rpc_url_str.parse().unwrap(),
+            contract_address: Address::from_str(address).unwrap(),
+            wallet: None
+        }
+    }
+
+    fn set_wallet(&mut self, wallet_key: &str) {
+        let signer_key =
+        SigningKey::from_slice(&hex::decode(wallet_key).unwrap()).expect("Invalid key");
+        let wallet = EthereumWallet::from(PrivateKeySigner::from_signing_key(signer_key));
+    
+        self.wallet = Some(wallet);
+    }
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    env_logger::init();
+
+    // Step 1: Read PEM and encode the input
+    let mut der_chain: Vec<Vec<u8>> = Vec::with_capacity(cli.cert_chain.len());
+    for path in cli.cert_chain.iter() {
+        let pem = std::fs::read(path)?;
+        let der = pem_to_der(&pem);
+        der_chain = [der_chain, der].concat();
+    }
+
+    // Step 2: Create a Chain instance
+    let mut chain = Chain::new(
+        remove_prefix_if_found(&cli.rpc_url), 
+        remove_prefix_if_found(&cli.address)
+    );
+
+    // Step 3
+    if cli.verify {
+        // A wallet is required to store the Journal on-chain
+        if let Some(wallet_key) = &cli.wallet {
+            chain.set_wallet(wallet_key.as_str());
+        } else {
+            return Err(Error::msg("Missing wallet key"));
+        }
+
+        let input = InputBytesType::abi_encode_params(&der_chain);
+        let seal = prove(&input)?;
+
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let transaction_receipt = rt.block_on(verify_cert_chain_proof(
+            chain, 
+            der_chain, 
+            &seal
+        ))?;
+        println!("Cert Chain Verified at https://testnet-explorer.ata.network/tx/0x{}", transaction_receipt.transaction_hash.to_string());
+
+    } else {
+        log::info!("Getting journal...");
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let journal = rt.block_on(read_journal(chain, der_chain))?;
+        println!("Journal: {:?}", journal);
+    }
+    
+    Ok(())
+}
+
+fn pem_to_der(pem_chain: &[u8]) -> Vec<Vec<u8>> {
+    let mut der_chain: Vec<Vec<u8>> = Vec::new();
+
+    for pem in Pem::iter_from_buffer(pem_chain) {
+        let current_pem_content = pem.unwrap().contents;
+        der_chain.push(current_pem_content);
+    }
+
+    der_chain
+}
+
+use X509VerifierDemo::Journal;
+sol! {
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    #[derive(Debug)]
+    X509VerifierDemo,
+    "artifacts/X509VerifierDemo.json"
+}
+
+async fn read_journal(chain_config: Chain, der_chain: Vec<Vec<u8>>) -> Result<Journal> {
+    let provider = ProviderBuilder::new().on_http(chain_config.rpc_url);
+    let contract = X509VerifierDemo::new(
+        chain_config.contract_address,
+        &provider
+    );
+
+    let mut encoded: Vec<Bytes> = Vec::with_capacity(der_chain.len());
+    for der in der_chain.iter() {
+        encoded.push(Bytes::copy_from_slice(&der));
+    }
+
+    let call_builder = contract.getVerifiedX509Journal(encoded);
+    let call_return = call_builder.call().await?;
+
+    Ok(call_return.journal)
+}
+
+async fn verify_cert_chain_proof(
+    chain_config: Chain,
+    der_chain: Vec<Vec<u8>>,
+    seal: &[u8]
+) -> Result<TransactionReceipt> {
+    let provider = ProviderBuilder::new()
+        .with_recommended_fillers()
+        .wallet(chain_config.wallet.unwrap())
+        .on_http(chain_config.rpc_url)
+    ;
+    let contract = X509VerifierDemo::new(
+        chain_config.contract_address,
+        &provider
+    );
+
+    let mut encoded: Vec<Bytes> = Vec::with_capacity(der_chain.len());
+    for der in der_chain.iter() {
+        encoded.push(Bytes::copy_from_slice(&der));
+    }
+
+    let tx_builder = contract.verifyX509ChainProof(
+        encoded, 
+        Bytes::copy_from_slice(seal)
+    );
+    let receipt = tx_builder.send().await?.get_receipt().await?;
+
+    Ok(receipt)
+}
+
+fn remove_prefix_if_found(h: &str) -> &str {
+    if h.starts_with("0x") {
+        &h[2..]
+    } else {
+        &h
+    }
+}
+
+fn prove(input: &[u8]) -> Result<Vec<u8>> {
+    let risc_zero_version =
+        std::env::var("RISC_ZERO_VERSION").unwrap_or_else(|_| "1.0.1".to_string());
+    let client = bonsai_sdk::Client::from_env(&risc_zero_version)?;
+
+    let image_id_hex = String::from(X509_IMAGE_ID);
+
+    // Prepare input data and upload it.
+    let input_id = client.upload_input(input.to_vec())?;
+
+    log::info!("InputID: {}", input_id);
+
+    // Start a session running the prover.
+    let session = client.create_session(image_id_hex, input_id, vec![])?;
+    log::info!("Prove session created, uuid: {}", session.uuid);
+    let _receipt = loop {
+        let res = session.status(&client)?;
+        if res.status == "RUNNING" {
+            std::thread::sleep(Duration::from_secs(15));
+            continue;
+        }
+        if res.status == "SUCCEEDED" {
+            log::info!("Prove session is successful!");
+            // Download the receipt, containing the output.
+            let receipt_url = res
+                .receipt_url
+                .context("API error, missing receipt on completed session")?;
+
+            log::info!("Receipt URL: {}", &receipt_url);
+
+            // break receipt;
+            break;
+        }
+
+        panic!(
+            "Workflow exited: {} | SessionId: {} | err: {}",
+            res.status,
+            session.uuid,
+            res.error_msg.unwrap_or_default()
+        );
+    };
+
+    // Fetch the snark.
+    let snark_session = client.create_snark(session.uuid)?;
+    log::info!(
+        "Proof to SNARK session created, uuid: {}",
+        snark_session.uuid
+    );
+    let snark_receipt = loop {
+        let res = snark_session.status(&client)?;
+        match res.status.as_str() {
+            "RUNNING" => {
+                std::thread::sleep(Duration::from_secs(15));
+                continue;
+            }
+            "SUCCEEDED" => {
+                log::info!("Snark session is successful!");
+                break res.output.context("No snark generated :(")?;
+            }
+            _ => {
+                panic!(
+                    "Workflow exited: {} | SessionId: {} | err: {}",
+                    res.status,
+                    snark_session.uuid,
+                    res.error_msg.unwrap_or_default()
+                );
+            }
+        }
+    };
+
+    let snark = snark_receipt.snark.to_vec();
+    let seal = groth16::encode(snark).context("Read seal")?;
+
+    Ok(seal)
+}
